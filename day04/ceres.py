01396ab3cad531ac834f79b5592b25f6ca796165
Florent Bouchez Tichadou <florent.bouchez-tichadou@imag.fr>
2024-12-06 23:04:55 +0100

    Initial commit for ceres

---
+++ day04/ceres.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+from AoC import *
+
+res1 = 0
+res2 = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    print ("Ligne:", line)
+
+print ("Valeur partie 1:", res1)
+print ("Valeur partie 2:", res2)
